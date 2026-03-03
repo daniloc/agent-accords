52cfba01cdc5307602997b0d37f1438859f6229e
Danilo Campos <mail@danilocampos.com>
2026-03-02 23:57:57 -0500

    Better link behavior

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,6 @@
 		{/if}
 	</a>
 	{#if item.type === 'skill'}
-		<a class="download-link" href="/{item.slug}/download"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="currentColor" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2" opacity="0.16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2"/></g></svg> Download in Agent Skill format (.zip)</a>
+		<a class="download-link" href="/{item.slug}/download" data-sveltekit-reload><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="currentColor" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2" opacity="0.16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2"/></g></svg> Download in Agent Skill format (.zip)</a>
 	{/if}
 {/each}
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -5,7 +5,7 @@
 
 <h1>{data.title}</h1>
 {#if data.type === 'skill'}
-	<a class="download-link detail" href="/{data.slug}/download"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="currentColor" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2" opacity="0.16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2"/></g></svg> Download in Agent Skill format (.zip)</a>
+	<a class="download-link detail" href="/{data.slug}/download" data-sveltekit-reload><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><path fill="currentColor" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2" opacity="0.16"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 17V5h7l2 2h9v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2"/></g></svg> Download in Agent Skill format (.zip)</a>
 {/if}
 
 <article class="card">
